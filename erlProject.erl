7f903a89a703191b3689a8eae083e45bb1f2dcef
Mohsin272 <mohsintahir999@gmail.com>
2023-03-21 23:55:54 +0000

    updated connectNode

--- erlProject.erl
+++ erlProject.erl
@@ -1,5 +1,5 @@
 -module(erlProject).
--export([computeNthPrime/1,ripNode/3,createNode/1,start/1,connectNode/4,setup/0,printTable/1]).
+-export([computeNthPrime/1,ripNode/3,createNode/1,start/1,connectNode/4,setup/0,printTable/1,computeNthPrime/4,printNeighbours/1]).
 
 setup()->
     [createNode(john),
@@ -24,29 +24,62 @@ connectNode(FirstNickname,FirstPID,SecondNickname,SecondPID)->
 
 printTable(PID)->
     PID!{printRoutingTable}.
+printNeighbours(PID)->
+    PID!{printNeighboursTable}.
+computeNthPrime(N,Sender,Desination,Hops)->
+    Sender ! {computeNthPrime,N,Sender,Desination,Hops}.
 start(Nickname)->
     register(Nickname,self()),
     ripNode(Nickname,[],[]).
 
 ripNode(MyNickname, NList, RTList) ->
     receive
-        {printRoutingTable}->
-            io:fwrite("~p ~p : Connected to node ~p ~n",[self(),MyNickname,RTList]),
-            ripNode(MyNickname,NList,RTList);
-        {computeNthPrime, N, Sender, Desination, Hops} ->
-            processMsgQuestion(N, Sender, Desination, Hops, NList, RTList);
-        {recieveAnswer, N, M, Sender, Desination, Hops} ->
-            processMsgAnswer(N, M, Sender, Desination, Hops);
+        % {printNeighbours} ->
+        %     io:fwrite("~p ~p : Neighbours Table ~p ~n", [self(), MyNickname, NList]),
+        %     ripNode(MyNickname, NList, RTList);
+        {printRoutingTable} ->
+            io:fwrite("~p ~p : Routing Table ~p ~n", [self(), MyNickname, RTList]),
+            ripNode(MyNickname, NList, RTList);
+        {computeNthPrime, N, Sender, Destination, Hops} ->
+            processMsgQuestion(N, Sender, Destination, Hops, NList, RTList);
+        {recieveAnswer, N, M, Sender, Destination, Hops} ->
+            processMsgAnswer(N, M, Sender, Destination, Hops);
         {updateRT, Sender, RTList} ->
             processRT(RTList);
-        {connect,Nickname,PID}->
-            io:fwrite("~p ~p : Connected to node ~p ~n",[self(),PID,Nickname]),
-            ripNode(MyNickname,NList ++ [{Nickname,PID}],RTList)
+        {connect, Nickname, PID} ->
+            io:fwrite("~p ~p : Connected to node ~p ~n", [self(), PID, Nickname]),
+            ripNode(MyNickname, NList ++ [{Nickname, PID}], RTList);
+        {printNeighboursTable} ->
+            lists:foreach(fun({Nickname, PID}) ->
+                                    io:fwrite("~p ~p: Neighbour ~p with PID ~p~n", [self(), MyNickname, Nickname, PID])
+                            end, NList),
+            ripNode(MyNickname, NList, RTList)
     end,
     ripNode(MyNickname, NList, RTList).
+    
+
+% ripNode(MyNickname, NList, RTList) ->
+%     receive
+%         {printNeighbours}->
+%             io:fwrite("~p ~p : Neighbours Table ~p ~n",[self(),MyNickname,NList]),
+%             ripNode(MyNickname,NList,RTList);
+%         {printRoutingTable}->
+%             io:fwrite("~p ~p : Routing Table ~p ~n",[self(),MyNickname,RTList]),
+%             ripNode(MyNickname,NList,RTList);
+%         {computeNthPrime, N, Sender, Desination, Hops} ->
+%             processMsgQuestion(N, Sender, Desination, Hops, NList, RTList);
+%         {recieveAnswer, N, M, Sender, Desination, Hops} ->
+%             processMsgAnswer(N, M, Sender, Desination, Hops);
+%         {updateRT, Sender, RTList} ->
+%             processRT(RTList);
+%         {connect,Nickname,PID}->
+%             io:fwrite("~p ~p : Connected to node ~p ~n",[self(),PID,Nickname]),
+%             ripNode(MyNickname,NList ++ [{Nickname,PID}],RTList)
+%     end,
+%     ripNode(MyNickname, NList, RTList).
 
 processMsgQuestion(N, Sender, Desination, Hops, NList, RTList) when Hops > 15 ->
-    true;
+    io:fwrite("~p ~p : Message to ~p is more than 15 hops ~n",[self(),Sender,Desination]);
 processMsgQuestion(N, Sender, MyNickname, Hops, NList, RTList) ->
     Answer = computeNthPrime(N),
     Neighbour = lookup(RTList, Sender),
@@ -67,11 +100,12 @@ processMsgQuestion(N, Sender, Desination, Hops, NList, RTList) ->
     end.
 
 lookup([], _) ->
-    notFound;
+    {notFound, 0};
 lookup([{Sender, Neighbour, Hops} | Tail], Sender) ->
-    Neighbour;
+    {Neighbour, Hops};
 lookup([{_, _, _} | Tail], Sender) ->
     lookup(Tail, Sender).
+    
 
 processMsgAnswer(N, M, Sender, Destination, Hops) ->
     io:format("Answer received: N=~p, M=~p, Sender=~p, Destination=~p, Hops=~p~n",[N, M, Sender, Destination, Hops]).
